ab29906142bdc8da3dd262f47b2f28d7f04960a7
Christian Cuna <christian.cuna89@gmail.com>
2022-08-15 17:25:58 +0300

    Add logo v0.1.0

--- app/components/layout/top-navigation/index.tsx
+++ app/components/layout/top-navigation/index.tsx
@@ -3,12 +3,14 @@ import { FC } from 'react';
 import { routeHrefs } from '~/constants/routes-hrefs';
 import LgScreenNavigation from './lg-screen-navigation';
 import XsScreenNavigation from './xs-screen-navigation';
+import logo from '/public/images/logo.png';
 
 const TopNavigation: FC = () => {
   return (
     <nav className='h-20 flex justify-center items-center'>
       <div className='basis-full flex justify-between items-center  px-4 lg:basis-5/6 xl:basis-4/6'>
-        <Link to={routeHrefs.home}>
+        <Link to={routeHrefs.home} className='flex gap-x-3 items-center'>
+          <img src={logo} className='h-10 w-10' />
           <h1 className='text-slate-200 text-4xl underline-on-hover'>
             mc cuna
           </h1>
--- app/root.meta.ts
+++ app/root.meta.ts
@@ -2,6 +2,6 @@ import { MetaFunction } from '@remix-run/cloudflare';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'mc cuna',
   viewport: 'width=device-width,initial-scale=1',
 });
