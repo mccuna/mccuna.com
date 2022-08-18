394a18c4f7eefa08eb839cbdd1ede2d6551c3378
Christian Cuna <christian.cuna89@gmail.com>
2022-08-18 23:15:30 +0300

    Update logo

--- app/components/layout/top-navigation/index.tsx
+++ app/components/layout/top-navigation/index.tsx
@@ -3,14 +3,13 @@ import { FC } from 'react';
 import { routeHrefs } from '~/constants/routes-hrefs';
 import LgScreenNavigation from './lg-screen-navigation';
 import XsScreenNavigation from './xs-screen-navigation';
-import logo from '/public/images/logo.png';
+import logo from '/public/images/logo.webp';
 
 const TopNavigation: FC = () => {
   return (
     <nav className='h-20 flex justify-center items-center'>
       <div className='basis-full flex justify-between items-center  px-4 lg:basis-5/6 xl:basis-4/6'>
         <Link to={routeHrefs.home} className='flex gap-x-3 items-center'>
-          {/* TODO: Convert logo to webp when final */}
           <img src={logo} className='h-10 w-10' />
           <h1 className='text-slate-200 text-4xl underline-on-hover hover:text-slate-50'>
             mc cuna
