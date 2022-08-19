9fd3e870009947de9bcac35326439cac2e0e8398
Christian Cuna <christian.cuna89@gmail.com>
2022-08-19 21:58:03 +0300

    Add transition to experience entry card

--- app/sections/experience/experience-entry-card/entry-body/index.tsx
+++ app/sections/experience/experience-entry-card/entry-body/index.tsx
@@ -1,4 +1,4 @@
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, Transition } from '@headlessui/react';
 import { FC } from 'react';
 import Card from '~/components/card';
 import { useExperienceEntry } from '../experience-entry-context';
@@ -9,19 +9,27 @@ const EntryBody: FC = () => {
   const experienceEntry = useExperienceEntry();
 
   return (
-    <Disclosure.Panel className='w-full'>
-      {({ close }) => (
-        <Card
-          variant='primary'
-          className='cursor-pointer rounded-t-none pt-0 group-hover:bg-indigo-700 group-hover:border-indigo-700'>
-          <div className='flex flex-col gap-y-5' onClick={() => close()}>
-            <ProjectInfo experienceEntry={experienceEntry} />
-            <hr className='text-slate-200' />
-            <CompanyInfo company={experienceEntry.company} />
-          </div>
-        </Card>
-      )}
-    </Disclosure.Panel>
+    <Transition
+      enter='transition duration-100 ease-out'
+      enterFrom='transform scale-95 opacity-0'
+      enterTo='transform scale-100 opacity-100'
+      leave='transition duration-75 ease-out'
+      leaveFrom='transform scale-100 opacity-100'
+      leaveTo='transform scale-95 opacity-0'>
+      <Disclosure.Panel className='w-full'>
+        {({ close }) => (
+          <Card
+            variant='primary'
+            className='cursor-pointer rounded-t-none pt-0 group-hover:bg-indigo-700 group-hover:border-indigo-700'>
+            <div className='flex flex-col gap-y-5' onClick={() => close()}>
+              <ProjectInfo experienceEntry={experienceEntry} />
+              <hr className='text-slate-200' />
+              <CompanyInfo company={experienceEntry.company} />
+            </div>
+          </Card>
+        )}
+      </Disclosure.Panel>
+    </Transition>
   );
 };
 
