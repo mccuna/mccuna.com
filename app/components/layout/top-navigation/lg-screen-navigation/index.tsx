773dce45e645c6911ea5bdfb051d5371aade6b39
Christian Cuna <christian.cuna89@gmail.com>
2022-09-13 23:11:18 +0300

    Add blog link to top-navigation

--- app/components/layout/top-navigation/lg-screen-navigation/index.tsx
+++ app/components/layout/top-navigation/lg-screen-navigation/index.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from '~/components/tooltip';
 import { routeHrefs } from '~/constants/routes-hrefs';
 import LgScreenNavigationLink from './lg-screen-navigation-link';
 
@@ -6,6 +7,11 @@ const LgScreenNavigation = () => {
     <ul className='hidden lg:flex justify-center items-center'>
       <LgScreenNavigationLink to={routeHrefs.home}>Home</LgScreenNavigationLink>
       <LgScreenNavigationLink to={routeHrefs.skills}>
+      <Tooltip title='Coming soon!' tooltipPlacement='bottom'>
+        <span className='w-full px-6 py-3 text-xl text-slate-600 hover:text-slate-200 cursor-pointer'>
+          Blog
+        </span>
+      </Tooltip>
         Skills
       </LgScreenNavigationLink>
       <LgScreenNavigationLink to={routeHrefs.experience}>
