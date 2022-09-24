225c9d96aea64a6279bc3b2512d10723b4781c25
Christian Cuna <christian.cuna89@gmail.com>
2022-09-24 12:01:24 +0300

    pw: fill contact form data

---
+++ tests/contact/index.spec.ts
@@ -0,0 +1,52 @@
+import {
+  locatorFixtures as fixtures,
+  LocatorFixtures as TestingLibraryFixtures,
+} from '@playwright-testing-library/test/fixture';
+import { test as base } from '@playwright/test';
+import fsPromises from 'fs/promises';
+
+const test = base.extend<TestingLibraryFixtures>(fixtures);
+const { expect } = test;
+
+test.describe('Contact page should', () => {
+  test('send an email when data is valid', async ({
+    page,
+    screen,
+    browserName,
+  }) => {
+    await page.goto('http://localhost:8787/contact');
+
+    const nameInput = await screen.findByRole('textbox', { name: /name/i });
+    await nameInput.fill('John Doe');
+
+    const emailInput = await screen.findByRole('textbox', { name: /email/i });
+    await emailInput.fill('john_doe@gmail.com');
+
+    const subjectInput = await screen.findByRole('textbox', {
+      name: /subject/i,
+    });
+    await subjectInput.type('Dummy email');
+
+    const messageTextbox = await screen.findByRole('textbox', {
+      name: /message/i,
+    });
+    await messageTextbox.fill(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    );
+
+    const screenshotBuffer = await page.screenshot({ fullPage: true });
+
+    await fsPromises.writeFile(
+      `tests/contact/screenshot_${browserName}.png`,
+      screenshotBuffer,
+    );
+
+    //   cy.findByTitle(
+    //     /widget containing checkbox for hcaptcha security challenge/i,
+    //   ).click();
+
+    //   cy.confirmCaptcha();
+
+    // cy.findByRole('button', { name: /send message/i }).click();
+  });
+});
