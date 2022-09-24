e5020712417d6e5fae1f0983360c7f454448303c
Christian Cuna <christian.cuna89@gmail.com>
2022-09-24 13:11:56 +0300

    setup playwright + miniflare

--- tests/contact/index.spec.ts
+++ tests/contact/index.spec.ts
@@ -1,32 +1,25 @@
-import {
-  locatorFixtures as fixtures,
-  LocatorFixtures as TestingLibraryFixtures,
-} from '@playwright-testing-library/test/fixture';
-import { test as base } from '@playwright/test';
-
-const test = base.extend<TestingLibraryFixtures>(fixtures);
-const { expect } = test;
+import { test } from '../setup';
 
 test.describe('Contact page should', () => {
   test('send an email when data is valid', async ({
     page,
-    screen,
+    queries,
     browserName,
   }) => {
     await page.goto('http://localhost:8787/contact');
 
-    const nameInput = await screen.findByRole('textbox', { name: /name/i });
+    const nameInput = await queries.findByRole('textbox', { name: /name/i });
     await nameInput.fill('John Doe');
 
-    const emailInput = await screen.findByRole('textbox', { name: /email/i });
+    const emailInput = await queries.findByRole('textbox', { name: /email/i });
     await emailInput.fill('john_doe@gmail.com');
 
-    const subjectInput = await screen.findByRole('textbox', {
+    const subjectInput = await queries.findByRole('textbox', {
       name: /subject/i,
     });
     await subjectInput.type('Dummy email');
 
-    const messageTextbox = await screen.findByRole('textbox', {
+    const messageTextbox = await queries.findByRole('textbox', {
       name: /message/i,
     });
     await messageTextbox.fill(
---
+++ tests/setup.ts
@@ -0,0 +1,67 @@
+import {
+  fixtures,
+  TestingLibraryFixtures,
+} from '@playwright-testing-library/test/fixture';
+import { expect, test as base } from '@playwright/test';
+import { Miniflare } from 'miniflare';
+import { MockAgent, setGlobalDispatcher } from 'undici';
+
+interface TestFixtures extends TestingLibraryFixtures {
+  mockAgent: MockAgent;
+}
+
+interface WorkerFixtures {
+  mf: Miniflare;
+  port: number;
+}
+
+export { expect };
+
+export const test = base.extend<TestFixtures, WorkerFixtures>({
+  // Setup queries from playwright-testing-library
+  ...fixtures,
+
+  // Assign a unique "port" for each worker process
+  port: [
+    async ({}, use, workerInfo) => {
+      await use(3001 + workerInfo.workerIndex);
+    },
+    { scope: 'worker' },
+  ],
+
+  // Setup mock client for requests initiated by the Worker
+  mockAgent: async ({}, use) => {
+    const mockAgent = new MockAgent();
+
+    setGlobalDispatcher(mockAgent);
+
+    await use(mockAgent);
+  },
+
+  // Miniflare instance
+  mf: [
+    async ({ port }, use) => {
+      const mf = new Miniflare({
+        wranglerConfigPath: true,
+        buildCommand: undefined,
+        bindings: {},
+        port,
+      });
+
+      // Start the server
+      const server = await mf.startServer();
+
+      // Use the server in the tests
+      await use(mf);
+
+      // Cleanup
+      await new Promise<void>((resolve, reject) => {
+        server.close((error) => (error ? reject(error) : resolve()));
+      });
+    },
+    {
+      scope: 'worker',
+      auto: true,
+    },
+  ],
+});
