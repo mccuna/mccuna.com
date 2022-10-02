ed4e25b0108d7ada8c87414e9ebe6887aae5c350
Christian Cuna <christian.cuna89@gmail.com>
2022-10-02 18:08:44 +0300

    fix about-me/my-journey title

--- app/routes/about-me.my-journey.meta.ts
+++ app/routes/about-me.my-journey.meta.ts
@@ -2,7 +2,7 @@ import { MetaFunction } from '@remix-run/cloudflare';
 import { getPageTitle } from '~/utils/meta-utils';
 
 export const meta: MetaFunction = () => ({
-  title: getPageTitle('About'),
+  title: getPageTitle('My journey'),
   description:
     'About Mihai Cristian Cună (mc cună). Read his software developer journey',
 });
