bb2cec749ba5f1b8f2820054d93db4c2750a9b39
Christian Cuna <christian.cuna89@gmail.com>
2022-12-26 14:05:14 +0200

    remove faunadb

--- app/utils/log-error.server.ts
+++ app/utils/log-error.server.ts
@@ -1,6 +1,3 @@
-import { query as q } from 'faunadb';
-import { executeQuery } from '~/helpers/fauna/execute-query';
-
 export type LogErrorArgs = {
   errorLog: unknown;
   config: {
@@ -13,21 +10,21 @@ export const logError = async ({
   config,
   errorLog,
 }: LogErrorArgs): Promise<void> => {
-  await executeQuery({
-    clientConfig: {
-      secret: config.faunaSecret,
-      domain: config.faunaDomain,
-      scheme: 'https',
-    },
-    query: q.Create(q.Collection('errors'), {
-      data: {
-        error: {
-          datetime: new Date().toISOString(),
-          value: typeof errorLog === 'object' ? { ...errorLog } : errorLog,
-        },
-      },
-    }),
-  });
+  // await executeQuery({
+  //   clientConfig: {
+  //     secret: config.faunaSecret,
+  //     domain: config.faunaDomain,
+  //     scheme: 'https',
+  //   },
+  //   query: q.Create(q.Collection('errors'), {
+  //     data: {
+  //       error: {
+  //         datetime: new Date().toISOString(),
+  //         value: typeof errorLog === 'object' ? { ...errorLog } : errorLog,
+  //       },
+  //     },
+  //   }),
+  // });
 };
 
 export const isJSError = (error: unknown): error is Error => {
